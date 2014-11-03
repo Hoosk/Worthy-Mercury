41b26ad67887ac418ba597e9d97c0941740e50f7
hoosk <odelgado@about.me>
2014-11-03 14:51:12 +0100

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace AwesomeGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pala = new System.Windows.Forms.PictureBox();
+            this.pelota = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pala)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pelota)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pala
+            // 
+            this.pala.BackColor = System.Drawing.Color.Transparent;
+            this.pala.BackgroundImage = global::AwesomeGame.Properties.Resources.arkanoid;
+            this.pala.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pala.Location = new System.Drawing.Point(238, 339);
+            this.pala.Name = "pala";
+            this.pala.Size = new System.Drawing.Size(89, 25);
+            this.pala.TabIndex = 0;
+            this.pala.TabStop = false;
+            // 
+            // pelota
+            // 
+            this.pelota.BackColor = System.Drawing.Color.Transparent;
+            this.pelota.Image = global::AwesomeGame.Properties.Resources.ball;
+            this.pelota.InitialImage = ((System.Drawing.Image)(resources.GetObject("pelota.InitialImage")));
+            this.pelota.Location = new System.Drawing.Point(268, 279);
+            this.pelota.Name = "pelota";
+            this.pelota.Size = new System.Drawing.Size(21, 22);
+            this.pelota.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pelota.TabIndex = 1;
+            this.pelota.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(413, -4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Vidas:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(520, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 25);
+            this.label2.TabIndex = 3;
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = global::AwesomeGame.Properties.Resources.hexagon_pattern;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(604, 384);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pelota);
+            this.Controls.Add(this.pala);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Arkanoid";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pala)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pelota)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pala;
+        private System.Windows.Forms.PictureBox pelota;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Resources;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AwesomeGame.Properties;
+using System.Threading;
+namespace AwesomeGame
+{
+    /*Adventure Time!!
+     * 
+     *   | (• ◡•)| (❍ᴥ❍ʋ)
+     *   
+           /|________________
+     O|===|*>________________ >
+           \|  
+     * 
+     *  Take your sword and debug!
+     */
+
+    /* = TODO LIST:
+     * - Testear y arreglar bugs tontos.
+     *  -Ahora ya acaba partida (crear metodo nextlvl).
+     *  -Que cada nivel sea mas complejo
+     * - Añadir BBDD.
+     * - Separarlo por capas.
+     * - Arreglar mas bugs tontos.
+     */
+    public partial class Form1 : Form
+    {
+        
+        /*Variables de posicion para la pala y la pelota y esas cosas*/
+        static int x = 238;
+        static int y = 339;
+        static int dx = 200;
+        static int dy = 200;
+
+        /*Objetos de imagenes */
+        ResourceManager rm = Resources.ResourceManager;
+
+        /*Random*/
+        static Random rand = new Random();
+
+        /*Variables de velocidad*/
+        static int velocidadx = 4;
+        static int velocidady = -4;
+
+        /*Vidas*/
+        static int vidas = 3;
+        /*Cantidad de filas y columnas*/
+        static int filas = 3;
+        static int columnas = 5;
+        /*variables de ladrillos*/
+        PictureBox[,] bloc = new PictureBox[4, 10];
+        static int numeroLadrillos = filas * columnas;
+        static int xx = 0;
+        static int yy = 25;
+        
+
+        public Form1()
+        {
+            
+            InitializeComponent();
+            crearblocs();
+            label2.Text = vidas.ToString();
+            timer1.Interval = 10;
+            timer2.Interval = 10;
+            timer1.Start();
+            timer2.Start();
+
+        }
+      
+        
+        /*Funcion para crear bloques*/
+        public void crearblocs()
+        {
+            for (int i = 1; i <= filas; i++)
+            {
+                for (int j = 1; j <= columnas; j++)
+                {
+                    bloc[i, j] = new PictureBox();
+                    bloc[i, j].Location = new Point(xx, yy);
+                    bloc[i, j].Image = (Bitmap)rm.GetObject("red");
+                    bloc[i, j].SizeMode = PictureBoxSizeMode.StretchImage;
+                    bloc[i, j].ClientSize = new Size((ClientSize.Width / 5), 20);
+                    bloc[i, j].BackColor = Color.Purple;                   
+                    
+                    this.Controls.Add(bloc[i, j]);
+                    xx += (ClientSize.Width / 5);
+                } xx = 0;
+                yy += 20;
+            }
+        }
+
+        /* Yo he visto KeyEvents que vosotros jamas creeríais. 
+         * Servidores en llamas más allá de Orión. 
+         * He visto Objective-C brillar en la oscuridad cerca de la Puerta de Visual Basic. 
+         * Todos esos momentos se perderán en el tiempo como strings en la variable $temp.
+         * Es hora de debugear."
+         */
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {            
+            switch (e.KeyChar)
+            {
+                case 'a':                    
+                    if (x >= 0) x -= 9;                    
+                    pala.Location = new Point(x,y);
+                    break;
+                case 'd':
+                    if (x <= this.ClientSize.Width - 92)x += 9;                   
+                    pala.Location = new Point(x, y);
+                    break;
+            }
+
+        }
+        
+        /*
+         * Un Timer para gobernarlos a todos, 
+         * un Timer para encontrarlos,
+         * un Timer para atraerlos a todos y atarlos en el dev/null. 
+         */
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //Thread.Sleep(25);
+            dx += velocidadx;
+            dy += velocidady;
+            pelota.Location = new Point(dx, dy);
+            if (pelota.Location.Y >= this.ClientSize.Height)
+            {
+                dx = this.ClientSize.Width/2;
+                dy = this.ClientSize.Height/2;
+                vidas -= 1;
+                velocidadx = 4;
+                velocidady = -4;
+                label2.Text = vidas.ToString();
+                if (vidas == 0)
+                {
+                    endGame();
+                }
+            }
+            if (numeroLadrillos == 0)
+            {
+                endGame();
+            }
+        }
+
+        
+        
+        /* El final del principio.
+         * 
+         */
+        private void endGame(){
+            timer1.Stop();
+            var result = MessageBox.Show("¿Deseas seguir jugando?", "¿Deseas seguir jugando?",
+                             MessageBoxButtons.YesNo,
+                             MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+
+                vidas = 3;
+                timer1.Start();
+            }
+            else
+            {
+                Form1.ActiveForm.Close();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Thread.Sleep(20);
+            //Comprobacion para ver si la pelota intersecciona con la pala o el techo
+            if (pelota.Bounds.IntersectsWith((pala.Bounds)) || pelota.Location.Y <= 0)
+            {
+                velocidady = -velocidady;
+            }
+            //Comprobacion para ver si la pelota intersecciona con los bordes del formulario
+            if (pelota.Location.X <= 0 || pelota.Location.X >= (this.ClientSize.Width - pelota.Width))
+            {
+                velocidadx = -velocidadx;
+            }
+
+            //Comprobacion de bloques para ver si la pelota ha colisionado o no.
+            for (int i = 1; i <= filas; i++)
+            {
+                for (int j = 1; j <= columnas; j++)
+                {
+                    if (pelota.Bounds.IntersectsWith(bloc[i, j].Bounds))
+                    {
+                        bloc[i, j].Dispose();
+                        bloc[i, j].SendToBack();
+                        bloc[i, j].SetBounds(0, 0, 0, 0);
+                        this.Controls.Remove(bloc[i, j]);
+                        numeroLadrillos -= 1;
+                        velocidady = -velocidady;
+                    }
+                }
+
+            }
+        }
+                
+    }
+}
